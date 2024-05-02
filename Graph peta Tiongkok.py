a8eafe1a6804010f83664172bf6aec28504d9235
Regha01010010 <147596855+Regha01010010@users.noreply.github.com>
2024-05-02 22:39:54 +0700

    Update Graph peta Tiongkok.py
    added @Regha01010010

--- Graph peta Tiongkok.py
+++ Graph peta Tiongkok.py
@@ -1,5 +1,5 @@
 """
-Maps Graph
+Maps Graph of Sichuan Province, China
 This program was made by kelompok 1 Struktur Data UNESA to fulfill project from our teacher Mr. I Gde Agung Sri Sidhimantra S.Kom M.Kom
 
 Our member
@@ -8,7 +8,7 @@ Our member
 3. Regha Rahmadian Bintang (156)
 
 See more on our github
-{github}
+https://github.com/SekarHanum/
 
 """
 class Peta:
@@ -38,3 +38,44 @@ class Peta:
         if kota1 and kota2 in self.listkota:
             del self.listkota[kota1][kota1]
             del self.listkota[kota2][kota1]
+            print(f'Jalan antara kota {kota1} dan kota {kota2} telah dihapus dari Peta')
+    
+    def hitungkota(self):
+        hitungan = 0
+        for kota in self.listkota:
+            if kota in self.listkota:
+                hitungan += 1
+        print(f'Jumlah kota yang ada pada peta ada {hitungan} kota')
+        return hitungan
+    
+    def printpeta(self):
+        for kota in self.listkota:
+            print(f'{kota} -> {self.listkota[kota]}')
+
+#Testzone
+PetaTiongkok = Peta()
+#Tambah Kota
+PetaTiongkok.tambahkota("Garze Tibetan")
+PetaTiongkok.tambahkota("Liangshan Yi")
+PetaTiongkok.tambahkota("Panzhihua")
+PetaTiongkok.tambahkota("Yaan")
+PetaTiongkok.tambahkota("Meishan")
+PetaTiongkok.tambahkota("Leshan")
+PetaTiongkok.tambahkota("Chengdu")
+PetaTiongkok.tambahkota("Deyang")
+PetaTiongkok.tambahkota("Dazhou")
+PetaTiongkok.tambahkota("Nanchong")
+PetaTiongkok.tambahkota("Guang'an")
+PetaTiongkok.tambahkota("Suining")
+PetaTiongkok.tambahkota("Ziyang")
+PetaTiongkok.tambahkota("Neijiang")
+PetaTiongkok.tambahkota("Zigong")
+PetaTiongkok.tambahkota("Yibin")
+PetaTiongkok.tambahkota("Luzhou")
+#Tambah Jalan
+PetaTiongkok.tambahJalan("Garze Tibetan","Yaan",132)
+PetaTiongkok.tambahJalan("Liangshan Yi","Panzhihua",215)
+PetaTiongkok.tambahJalan("Liangshan Yi","Yaan",310)
+PetaTiongkok.tambahJalan("Yaan","Chengdu",132)
+PetaTiongkok.tambahJalan("Yaan","Meishan",103)
+
